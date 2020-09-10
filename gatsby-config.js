62c803994cc4409f17c570a54dfa16c08f0a9e0d
J Holt <jordan.su@protonmail.com>
2020-09-10 09:54:09 -0400

    ⚙	modified:   gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: process.env.GOOGLE_TRACKING_ID,
+        head: true
       },
     },
     {
